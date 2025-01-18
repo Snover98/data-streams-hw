f603a6ac1109ceb837447c3ab90148e551b5c7c6
Snover98 <Assaf98@Gmail.com>
2025-01-18 16:55:23 +0200

    minor: improved performance of experiments & made sure to call the more primitive estimators when `num_betas == 1`

--- fm/experiments.py
+++ fm/experiments.py
@@ -4,6 +4,8 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 from tqdm import tqdm
 import numpy as np
 
+from alpha_fm import AlphaFm
+from beta_fm import BetaFm
 from full_fm import FullFm
 
 from typing import Callable
@@ -14,23 +16,30 @@ def random_hash(value: int) -> float:
 
 def seeded_hash(hash_seed: int) -> Callable[[int], float]:
     def sub_hash(value: int) -> float:
-        return random_hash(value + hash_seed * 53)
+        return random_hash(value + hash_seed)
     return sub_hash
 
 
 def run_experiment(values: np.ndarray, base_seed: int, num_hashes: int, num_betas: int) -> float:
     generator = np.random.default_rng(seed=base_seed)
-    hash_funcs = [seeded_hash(generator.integers(1, 1000)) for _ in range(num_hashes)]
-    ams = FullFm(hash_funcs, len(hash_funcs) // num_betas)
-    return float(ams.update(values).estimate())
+    hash_seeds = generator.choice(np.arange(1, 1001), num_hashes, replace=False)
+    hash_funcs = [seeded_hash(seed) for seed in hash_seeds]
+
+    if num_betas == 1 and num_hashes == 1:
+        fm = AlphaFm(hash_funcs[0])
+    elif num_betas == 1:
+        fm = BetaFm(hash_funcs)
+    else:
+        fm = FullFm(hash_funcs, len(hash_funcs) // num_betas)
+    return fm.update(values).estimate()
 
 
-def run_experiments(values: np.ndarray, base_seeds: list[int], num_hashes: int, num_betas: int, num_procs: int = 6) -> list[float]:
+def run_experiments(values: np.ndarray, base_seeds: list[int], num_hashes: int, num_betas: int, num_procs: int = 0) -> list[float]:
     if not num_procs:
         return [run_experiment(values, s, num_hashes, num_betas) for s in tqdm(base_seeds)]
 
     with ProcessPoolExecutor(max_workers=num_procs) as executor:
         futures = [executor.submit(run_experiment, values, s, num_hashes, num_betas) for s in base_seeds]
-        results =  [f.result() for f in tqdm(futures)]
+        results =  [f.result() for f in tqdm(as_completed(futures), total=len(futures))]
 
     return results
