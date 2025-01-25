7aa98856dab1451269c4d88a044a9a370972b831
Snover98 <Assaf98@Gmail.com>
2025-01-25 11:02:35 +0200

    feature: changed the way `FullFm` divides the hash functions to make sure that we don't have a starved beta estimator in the end - it now instead tries to minimize the difference between the beta estimator with most hash functions and the one with least

--- fm/full_fm.py
+++ fm/full_fm.py
@@ -6,13 +6,29 @@ from typing import Callable, Iterable, Sequence, TypeVar
 
 T = TypeVar("T")
 
-def chunk(vals: Sequence[T], chunk_size: int) -> Iterable[Sequence[T]]:
-    for idx in range(0, len(vals), chunk_size):
-        yield vals[idx:idx+chunk_size]
+def calculate_chunk_sizes(seq_len: int, num_chunks: int) -> list[int]:
+    res = np.zeros(num_chunks).astype(int)
+    cur_len = num_chunks
+    num_left = seq_len
+
+    while np.sum(res) < seq_len:
+        addition = num_left // cur_len
+        if np.sum(res) + addition * cur_len <= seq_len:
+            res[np.arange(cur_len)] += addition
+            num_left -= addition * cur_len
+        cur_len -= 1
+
+    return res.tolist()
+
+def chunk(vals: Sequence[T], num_chunks: int) -> Iterable[Sequence[T]]:
+    cur_start = 0
+    for chunk_size in calculate_chunk_sizes(len(vals), num_chunks):
+        yield vals[cur_start:cur_start+chunk_size]
+        cur_start += chunk_size
 
 class FullFm:
-    def __init__(self, hash_funcs: list[Callable[[int], float]], alphas_per_beta: int):
-        self.estimators: tuple[BetaFm, ...] = tuple(BetaFm(funcs) for funcs in chunk(hash_funcs, alphas_per_beta))
+    def __init__(self, hash_funcs: list[Callable[[int], float]], num_betas: int):
+        self.estimators: tuple[BetaFm, ...] = tuple(BetaFm(funcs) for funcs in chunk(hash_funcs, num_betas))
     
     def update(self, values: np.ndarray) -> 'FullFm':
         for est in self.estimators:
